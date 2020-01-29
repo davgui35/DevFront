9e683f0d98b52b4d6cea96eff0d2cb4568f58751
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-01-29 12:14:58 +0100

    for

--- monjs.js
+++ monjs.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('h1').hide(5000);
+
+    for(var i = 0; i < 5; i++) {
+        $('<div class="col-1 boite>').appendTo('#effets')
+    }
+
 });
+
+//Pour faire disparaitre le h1
+//5000 temps en secondes
