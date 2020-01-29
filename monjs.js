efd6b90708b6c99feff01895092d1cab29cb2ff4
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-01-29 12:31:22 +0100

    3effets

--- monjs.js
+++ monjs.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     // $('h1').hide(5000);
-
+   //Pour faire disparaitre le h1
+//5000 temps en secondes
     for(var i = 0; i < 5; i++) {
         $('<div class="col-1 boite">').appendTo('#effets');
     }
+ 
 //ajoute 5 fois la boite rouge
 $("#effets div").click(function() {
  $( this ).hide( 2000, function() {
@@ -11,5 +13,4 @@ $("#effets div").click(function() {
     });
 });
 
-//Pour faire disparaitre le h1
-//5000 temps en secondes
+});
