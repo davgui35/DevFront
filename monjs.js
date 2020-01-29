08256971571328e66c84990f13c5cb661f2bf1e0
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-01-29 12:30:17 +0100

    2effets

--- monjs.js
+++ monjs.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-    $('h1').hide(5000);
+    // $('h1').hide(5000);
 
     for(var i = 0; i < 5; i++) {
-        $('<div class="col-1 boite">').appendTo('#effets')
+        $('<div class="col-1 boite">').appendTo('#effets');
     }
 //ajoute 5 fois la boite rouge
 $("#effets div").click(function() {
  $( this ).hide( 2000, function() {
     $( this ).remove();
     });
-});
 });
 
 //Pour faire disparaitre le h1
