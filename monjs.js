e3c2f0c40e0365d8464929882d891e11f14bb06a
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-01-29 12:16:21 +0100

    for2

--- monjs.js
+++ monjs.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('h1').hide(5000);
 
     for(var i = 0; i < 5; i++) {
-        $('<div class="col-1 boite>').appendTo('#effets')
+        $('<div class="col-1 boite">').appendTo('#effets')
     }
-
+//ajoute 5 fois la boite rouge
 });
 
 //Pour faire disparaitre le h1
